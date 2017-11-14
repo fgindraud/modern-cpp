cebce9e2cb9179a5a53ccd5f037eb071e96a57aa
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-11-14 10:44:31 +0100

    s4: list example for unique_ptr

--- session-4-smartpointers/motiv-unique.cpp
+++ session-4-smartpointers/motiv-unique.cpp
@@ -1,59 +1,34 @@
-#include <stdlib.h>
-#include <time.h>
 #include <iostream>
-#include <numeric>
-#include <vector>
-#include <memory>
 
-struct Animal {
-    virtual int nb_legs() = 0;
-    virtual ~Animal() = default;
-};
-
-struct Panda : public Animal {
-    virtual int nb_legs() override { return 4; }
-};
-
-struct Spider : public Animal {
-    virtual int nb_legs() override { return 8; }
-};
-
-class GroupOfAnimals {
-    // std::vector<Animal*> animals;
-    std::vector<std::unique_ptr<Animal>> animals;
+class IntList {
+    int value;
+    IntList* rest;
 
   public:
-    GroupOfAnimals(int number) {
-        for (int i = 0; i < number; i++)
-            if (rand() % 2 == 1)
-                animals.emplace_back(new Spider());
-                // animals.push_back(new Spider());
-            else
-                animals.emplace_back(new Panda());
-                // animals.push_back(new Panda());
+    IntList(int value, IntList* rest = nullptr) : value(value), rest(rest) {}
+
+    void insert(int new_value) {
+        rest = new IntList(value, rest);
+        value = new_value;
     }
 
-    int nb_legs() {
-        int acc = 0;
-        // for (auto ptr : animals) {
-        //     acc += ptr->nb_legs();
-        // }
-        for (auto& ptr : animals) {
-            acc += ptr->nb_legs();
-        }
-        return acc;
-    }
+    int get() { return value; }
 
-    // ~GroupOfAnimals() {
-    //     for (auto ptr : animals) {
-    //         delete ptr;
-    //     }
-    // }
+    IntList* next() { return rest; }
+
+    ~IntList() {
+        delete rest;
+    }
 };
 
 int main() {
-    srand(time(NULL));
+    IntList l(1);
+    l.insert(3);
+    l.insert(17);
 
-    GroupOfAnimals group(50);
-    std::cout << group.nb_legs() << '\n';
+    auto it = &l;
+    while (it != nullptr) {
+        std::cout << it->get() << '\n';
+        it = it->next();
+    }
 }
