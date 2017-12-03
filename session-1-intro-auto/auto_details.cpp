a1bb8c78fadc0f3fdf26ad7a25bbc5c6ea1289ac
François Gindraud <francois.gindraud@gmail.com>
2017-12-03 17:02:43 +0100

    Simplified examples, clang format
    Removed C++11 stuff that is only explained later (init_list, emplace).
    Added "not explained" tags for type traits.

--- session-1-intro-auto/auto_details.cpp
+++ session-1-intro-auto/auto_details.cpp
@@ -4,64 +4,73 @@
 #include <type_traits>
 
 #ifdef BASIC_TYPES
-int main() {
-    std::cout << std::boolalpha;
-    auto i = 42;  // int
-    std::cout << std::is_same<decltype(i), int>::value << "\n";
+int main () {
+	std::cout << std::boolalpha;
+	auto i = 42; // int
+	std::cout << std::is_same<decltype (i), int>::value << "\n";
 
-    auto s = "hello";
-    std::cout << std::boolalpha << std::is_same<decltype(s), const char*>::value << "\n";
-    auto& s_ref = "hello";
-    std::cout << std::is_same<decltype(s_ref), const char(&)[6]>::value << "\n";
-    return 0;
+	auto s = "hello";
+	std::cout << std::boolalpha << std::is_same<decltype (s), const char *>::value << "\n";
+	auto & s_ref = "hello";
+	std::cout << std::is_same<decltype (s_ref), const char(&)[6]>::value << "\n";
+	return 0;
 }
 #endif
 
+// Build a dummy int vector
+std::vector<int> makeIntVector (int n) {
+	std::vector<int> r;
+	for (int i = 0; i < n; ++i)
+		r.push_back (i + 1);
+	return r;
+}
+
 #ifdef AUTO_INIT
-std::vector<int> makeIntVector() { return {1, 2, 3, 4}; }
+int main () {
+	// auto enables a new pattern for initialisation:
+	auto vec = std::vector<int> (100, 42);
 
-int main() {
-    // auto enables a new pattern for initialisation:
-    auto vec = std::vector<int>{1, 2, 3, 4};
+	// The C++11 spec allows the compiler to transform this into:
+	std::vector<int> vec_ (100, 42);
+	// (removes the copy entirely, even if the code is non trivial)
+	// Most reasonnable compilers will do it.
+	// (in the auto_rules.cpp/AUTO example, NoisyInt(4) was never copied)
+	// Since C++17, compilers are required to remove the copies.
 
-    // The C++11 spec allows the compiler to transform this into:
-    std::vector<int> vec{1, 2, 3, 4};
-    // (removes the copy entirely, even if the code is non trivial)
-    // Most reasonnable compilers will do it.
-    // Since C++17, compilers are required to remove the copies
-
-    // auto is very useful to remove redundant information when using factory functions:
-    auto vec2 = makeIntVector();
-    // instead of
-    std::vector<int> vec3 = makeIntVector();
-    // it also allows to change the type of returned objects without having to change call sites
+	// auto is very useful to remove redundant information when using factory functions:
+	auto vec2 = makeIntVector (42);
+	// instead of
+	std::vector<int> vec3 = makeIntVector (42);
+	// it also allows to change the type of returned objects without having to change call sites
 }
 #endif
 
 #ifdef FUNCTION_ARG
 // auto CANNOT be used as function arguments
-void f(auto i) { std::cout << i; }
+void f (auto i) {
+	std::cout << i;
+}
 
 // beware, as g++ allows it as an extension since C++14. This fails in clang++.
 #endif
 
 #ifdef TEMPLATE
 // auto in arguments is covered by function templates, since before C++11:
-template <typename T>
-void f(T i) {
-    std::cout << i << "\n";
+template <typename T> void f (T i) {
+	std::cout << i << "\n";
 }
 
 // auto is practical in templates: you can declare local variables easily:
-template <typename Container>
-void print_front(const Container& container) {
-    auto& v = container.front();  // instead of Container::const_reference f = container.front ()
-    std::cout << v << "\n";
+template <typename Container> void print_front (const Container & container) {
+	auto & v = container.front (); // instead of Container::const_reference f = container.front ()
+	std::cout << v << "\n";
 }
 
-int main() {
-    f(42);
-    print_front(std::vector<int>{1, 2, 3});
+int main () {
+	f (42);
+
+	auto v = makeIntVector (42);
+	print_front (v);
 }
 #endif
 
@@ -69,27 +78,33 @@ int main() {
 // C++14 allows deducted return type, if this is not ambiguous
 
 // Follows the same rules as auto variable: this returns an int, not a const int&
-auto front(const std::vector<int>& v) { return v.front(); }
+auto front (const std::vector<int> & v) {
+	return v.front ();
+}
 
 // Force reference return:
-auto& front_ref(const std::vector<int>& v) { return v.front(); }
+auto & front_ref (const std::vector<int> & v) {
+	return v.front ();
+}
 
 // Ask for "raw" type deduction, will return exactly the same expression type:
-decltype(auto) front_raw(const std::vector<int>& v) { return v.front(); }
+decltype (auto) front_raw (const std::vector<int> & v) {
+	return v.front ();
+}
 
-int main() {
-    std::vector<int> ints{1, 2, 3, 4};
+int main () {
+	auto ints = makeIntVector (42);
 
-    std::cout << front(ints) << "\n";
-    std::cout << std::boolalpha << std::is_same<decltype(front(ints)), int>::value << "\n";
+	std::cout << front (ints) << "\n";
+	std::cout << std::boolalpha << std::is_same<decltype (front (ints)), int>::value << "\n";
 
-    std::cout << front_ref(ints) << "\n";
-    std::cout << std::boolalpha << std::is_same<decltype(front_ref(ints)), const int&>::value
-              << "\n";
+	std::cout << front_ref (ints) << "\n";
+	std::cout << std::boolalpha << std::is_same<decltype (front_ref (ints)), const int &>::value
+	          << "\n";
 
-    std::cout << front_raw(ints) << "\n";
-    std::cout << std::boolalpha << std::is_same<decltype(front_raw(ints)), const int&>::value
-              << "\n";
+	std::cout << front_raw (ints) << "\n";
+	std::cout << std::boolalpha << std::is_same<decltype (front_raw (ints)), const int &>::value
+	          << "\n";
 }
 
 /* Personally, I think return type deduction is only useful for very tiny wrappers.
@@ -102,14 +117,14 @@ int main() {
 // Auto return type not available in c++11
 
 // An ugly way to simulate it is to use decltype again:
-auto front(const std::vector<int>& v) -> decltype(v.front()) { return v.front(); }
+auto front (const std::vector<int> & v) -> decltype (v.front ()) {
+	return v.front ();
+}
 // The function body is duplicated...
 
-int main() {
-    std::vector<int> ints{1, 2, 3, 4};
-    std::cout << front(ints) << "\n";
-    std::cout << std::boolalpha << std::is_same<decltype(front(ints)), const int&>::value << "\n";
+int main () {
+	auto ints = makeIntVector (42);
+	std::cout << front (ints) << "\n";
+	std::cout << std::boolalpha << std::is_same<decltype (front (ints)), const int &>::value << "\n";
 }
 #endif
-
-// pattern of auto a = T(...)
--- session-1-intro-auto/auto_intro.cpp
+++ session-1-intro-auto/auto_intro.cpp
@@ -3,66 +3,70 @@
 #include <string>
 #include <vector>
 
-#include <type_traits>
+#include <type_traits> // tools for comparing types, not explained
 
-int main() {
-    std::cout << std::boolalpha;
+int main () {
+	std::cout << std::boolalpha;
 #ifdef MAP_FIND
-    {
-        // map: string -> vector<int>
-        std::map<std::string, std::vector<int>> my_map;
-        my_map.emplace("a", std::vector<int>{1, 2, 3, 4});
-        my_map.emplace("b", std::vector<int>{0, 0});
+	{
+		// map: string -> vector<int>
+		std::map<std::string, std::vector<int>> my_map;
+		std::vector<int> v;
+		v.push_back (42);
+		my_map.insert (std::make_pair ("a", v));
 
-        // find returns an iterator
-        // type is veeeeeerrrryyyy long
-        std::map<std::string, std::vector<int>>::iterator it = my_map.find("a");
-        std::cout << "my_map[a].size ()=" << it->second.size() << "\n";
+		// find returns an iterator
+		// type is veeeeeerrrryyyy long
+		std::map<std::string, std::vector<int>>::iterator it = my_map.find ("a");
+		std::cout << "my_map[a].front ()=" << it->second.front () << "\n";
 
-        // before C++11, you could shorten it by using a typedef
+		// before C++11, you could shorten it by using a typedef
 
-        // C++11 introduced (in fact, reused) the "auto keyword" to make LOCAL type deductions:
-        auto it2 = my_map.find("a");
-        std::cout << "my_map[a].size ()=" << it2->second.size() << "\n";
-        // it2 has the same type as it, without the long declaration
+		// C++11 introduced (in fact, reused) the "auto keyword" to make LOCAL type deductions:
+		auto it2 = my_map.find ("a");
+		std::cout << "my_map[a].front ()=" << it2->second.front () << "\n";
+		// it2 has the same type as it, without the long declaration
 
-        // check that using type_traits
-        // (decltype (expr) returns the type of the expression)
-        std::cout << std::is_same<decltype(it), decltype(it2)>::value << "\n";
-    }
+		// check that it and it2 have the same type using type_traits
+		// (decltype (expr) returns the type of the expression)
+		std::cout << std::is_same<decltype (it), decltype (it2)>::value << "\n";
+	}
 #endif
 #ifdef VECTOR
-    {
-        // simplifies iterator loops
-        std::vector<int> ints{1, 2, 3, 4, 5};
+	{
+		// simplifies iterator loops
+		std::vector<int> ints;
+		ints.push_back (1);
+		ints.push_back (2);
+		ints.push_back (3);
 
-        for (std::vector<int>::iterator it = ints.begin(); it != ints.end(); ++it)
-            std::cout << *it << ",";
-        std::cout << "\n";
+		for (std::vector<int>::iterator it = ints.begin (); it != ints.end (); ++it)
+			std::cout << *it << ",";
+		std::cout << "\n";
 
-        for (auto it = ints.begin(); it != ints.end(); ++it) std::cout << *it << ",";
-        std::cout << "\n";
+		for (auto it = ints.begin (); it != ints.end (); ++it)
+			std::cout << *it << ",";
+		std::cout << "\n";
 
-        // If you want to force use of const_iterator, use cbegin():
-        std::vector<int>::const_iterator const_it_old_version = ints.begin();
-        auto const_it_with_auto = ints.cbegin();
+		// If you want to force use of const_iterator, use cbegin():
+		std::vector<int>::const_iterator const_it_old_version = ints.begin ();
+		auto const_it_with_auto = ints.cbegin ();
 
-        // type_trait check
-        std::cout
-            << std::is_same<decltype(const_it_old_version), decltype(const_it_with_auto)>::value
-            << "\n";
+		// type_trait check
+		std::cout << std::is_same<decltype (const_it_old_version), decltype (const_it_with_auto)>::value
+		          << "\n";
 
-        // auto by default copies the values (see auto_rules.cpp for details)
-        // there are reference versions:
-        int& front_explicit = ints[0];
-        auto& front_auto = ints[0];
-        std::cout << front_explicit << " " << front_auto << "\n";
-        front_auto = 42;
-        // const references as well
-        const int& cfront_explicit = ints[0];
-        auto& cfront_auto = ints[0];
-        std::cout << cfront_explicit << " " << cfront_auto << "\n";
-    }
+		// auto by default copies the values (see auto_rules.cpp for details)
+		// there are reference versions:
+		int & front_explicit = ints[0];
+		auto & front_auto = ints[0];
+		std::cout << front_explicit << " " << front_auto << "\n";
+		front_auto = 42;
+		// const references as well
+		const int & cfront_explicit = ints[0];
+		auto & cfront_auto = ints[0];
+		std::cout << cfront_explicit << " " << cfront_auto << "\n";
+	}
 #endif
-    return 0;
+	return 0;
 }
