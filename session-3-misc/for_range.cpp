b6f2782a671c2a959024a2a9f6bffbf0afdb061d
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-12-05 10:40:44 +0100

    Some work on for_range and init_list

--- session-3-misc/for_range.cpp
+++ session-3-misc/for_range.cpp
@@ -8,42 +8,42 @@ using namespace std;
 int main() {
     vector<int> v{2, 3, 1, 6, 7, 1};
 
-    // integer index
     for (int i = 0; i < v.size(); i++) {
         cout << v.at(i) << ' ';
     }
     cout << '\n';
 
-    // what about sets?
-    set<int> s{1, 3, 7, 5};
+    // set<int> s{1, 3, 7, 5};
 
-    // iterators
-    for (set<int>::iterator it = s.begin(); it != s.end(); ++it) {
-        cout << *it << ' ';
-    }
-    cout << '\n';
+    // for (set<int>::iterator it = s.begin(); it != s.end(); ++it) {
+    //     cout << *it << ' ';
+    // }
+    // cout << '\n';
 
-    for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-        cout << *it << ' ';
-    }
-    cout << '\n';
+    // for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+    //     cout << *it << ' ';
+    // }
+    // cout << '\n';
 
-    // range-based for
-    for (const int element : s) {
-        cout << element << ' ';
-    }
-    cout << '\n';
+    // for (const int element : s) {
+    //     cout << element << ' ';
+    // }
+    // cout << '\n';
 
-    for (auto element : s) {
-        cout << element << ' ';
-    }
-    cout << '\n';
+    // for (auto element : s) {
+    //     cout << element << ' ';
+    // }
+    // cout << '\n';
 
-    // with maps
-    map<string, int> m{make_pair("a", 1), make_pair("b", 3), make_pair("c", 17)};
+    // map<string, int> m{make_pair("a", 1), make_pair("b", 3), make_pair("c", 17)};
 
-    for (auto p : m) {
-        cout << '(' << p.first << ", " << p.second << ") ";
-    }
-    cout << '\n';
+    // for (auto p : m) {
+    //     cout << '(' << p.first << ", " << p.second << ") ";
+    // }
+    // cout << '\n';
+
+    // for (auto i : {1, 2, 3, 17}) {
+    //     cout << i << ' ';
+    // }
+    // cout << '\n';
 }
--- session-3-misc/init_list.cpp
+++ session-3-misc/init_list.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-// void display_vec(vector<int> v) {
-//     for (size_t i = 0; i < v.size(); i++) {
-//         cout << v[i] << ' ';
-//     }
-//     cout << '\n';
-// }
+void display_vec(vector<int> v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+    cout << '\n';
+}
 
 int main() {
     vector<int> v;
@@ -19,6 +19,7 @@ int main() {
     v.push_back(7);
 
     // vector<int> v{1, 4, 11, 12, 3, 4, 5, 1, 7, 23, 12};
+    display_vec(v);
 
     // map<int, string> m = {{2, "aa"}, {5, "youpi"}, {98, "youpla"}};
 
@@ -26,4 +27,9 @@ int main() {
     //     cout << it->first << "/" << it->second << "  ";
     // }
     // cout << '\n';
+
+    // vector<int> v2(12, 2);
+    // vector<int> v2{12, 2};
+    // vector<int> v2 = {12, 2};
+    // display_vec(v2);
 }
